b08dc4efda2c3010e5c9792422afbc7276053514
BobbydpStrange <51657838+BobbydpStrange@users.noreply.github.com>
2022-09-26 18:49:28 -0600

    added a razor page

---
+++ web/Web/Pages/form.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.Pages
+{
+    public class formModel : PageModel
+    {
+        
+
+        public void OnGet()
+        {
+        }
+    }
+}
